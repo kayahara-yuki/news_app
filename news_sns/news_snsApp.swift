4fdc6d0e47e5d98ae500c18763ba153f6163a620
kayahara-yuki <yuanbutaye@icloud.com>
2025-10-07 11:25:09 +0900

    Initial Commit

---
+++ news_sns/news_snsApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_snsApp.swift
+//  news_sns
+//
+//  Created by zeroplus-shere2 on 2025/10/07.
+//
+
+import SwiftUI
+
+@main
+struct news_snsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
